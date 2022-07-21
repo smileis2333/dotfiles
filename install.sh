ee39fa09d13bea54c7229723b5425e7e7cc247b6
smileis2333 <43066818+smileis2333@users.noreply.github.com>
2022-07-21 15:21:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-echo 123
-echo $(pwd)
-echo kk>>kk.txt
-
 wget https://dlcdn.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar xzvf apache-maven-3.6.3-bin.tar.gz
-export M2_HOME=/home/gitpod/apache-maven-3.6.3
